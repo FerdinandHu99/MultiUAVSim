6bae52b6841e1fd4da73babaef04971494d081c1
ferdinand <853712064@qq.com>
2024-01-14 21:33:11 +0800

    feat: 加入AirSim插件
    初始化项目

---
+++ Source/MultiUAVSim/MultiUAVSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiUAVSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIUAVSIM_API AMultiUAVSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
